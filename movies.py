d0ea8af37de76671c54af3752734f6d4773497c7
Spence Perry <spence.perry@gmail.com>
2020-05-27 12:11:28 -0400

    add rough matching for movie/show years and minor styling improvements

--- movies.py
+++ movies.py
@@ -6,21 +6,27 @@ import json
 import time
 import urllib
 import yaml
+from blessings import Terminal
+
+#Set time range
+TIME_WINDOW = "week"
+
+term = Terminal()
 
 with open('config.yml') as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-reddit_client_id=cfg["reddit"]["client_id"]
-reddit_client_secret=cfg["reddit"]["client_secret"]
-reddit_user_agent=cfg["reddit"]["user_agent"]
-
+REDDIT_CLIENT_ID=cfg["reddit"]["client_id"]
+REDDIT_CLIENT_SECRET=cfg["reddit"]["client_secret"]
+REDDIT_USER_AGENT=cfg["reddit"]["user_agent"]
+TIME_WINDOW = cfg["reddit"]["time_window"]
 
 rt_url_base = 'https://www.rottentomatoes.com/api/private/v1.0/'
 headers = {'Content-Type': 'application/json'}
 
-reddit = praw.Reddit(client_id=reddit_client_id,
-                    client_secret=reddit_client_secret,
-                    user_agent=reddit_user_agent)
+reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
+                    client_secret=REDDIT_CLIENT_SECRET,
+                    user_agent=REDDIT_USER_AGENT)
 
 #For comparison https://www.rottentomatoes.com/api/private/v1.0/search?q=cats
 
@@ -33,7 +39,7 @@ class movies:
         self.rt_score = rt_score
 
 allMovies = []
-submissions = reddit.subreddit('NetflixBestOf').search("[US] -title:[REQUEST]", time_filter='week')
+submissions = reddit.subreddit('NetflixBestOf').search("[US] -title:[REQUEST]", time_filter=TIME_WINDOW)
 for submission in submissions:
     postTitle = submission.title
     score = submission.score
@@ -50,7 +56,7 @@ for submission in submissions:
     if not movieYearSearch is None:
         movieYear = movieYearSearch.group(0)
     if not movieNameSearch is None:
-        movieName = movieNameSearch.group(0)
+        movieName = movieNameSearch.group(0)[:-1]
     else:
         movieName = ""
     if not movieDescSearch is None:
@@ -66,20 +72,18 @@ for submission in submissions:
 
 
 def find_score(title, year):
-    #to do: remove special characters from title query completely?
     safe_title = urllib.parse.quote(title, safe='')
     api_url = '{rt_url_base}search?q={title}'.format(rt_url_base=rt_url_base, title=safe_title)
     response = requests.get(api_url)
     json_data = json.loads(response.text)
+
     movie_results = json_data["movies"]
     tv_results = json_data["tvSeries"]
     matched_score=-1
 
-    #to do: add fuzzy logic for approximate year?
-
-
+    #try release year supplied from reddit
     if json_data["movieCount"] > 0:
-        e = 'Searching for {title} in Movies'.format(title=title)
+        e = 'Searching for {title} ({year}) in Movies'.format(title=title, year=year)
         print(e)
 
         for result in movie_results:
@@ -87,7 +91,7 @@ def find_score(title, year):
             if int(result_year) == int(year) and 'meterScore' in result:
                 matched_score += result["meterScore"] + 1
     else:
-        e = 'Searching for {title} in TV Shows'.format(title=title)
+        e = 'Searching for {title} ({year}) in TV Shows'.format(title=title, year=year)
         print(e)
 
     if json_data["tvCount"] > 0:
@@ -100,20 +104,49 @@ def find_score(title, year):
                 matched_score += result["meterValue"] + 1
             if int(endYear) == int(year) and 'meterValue' in result:
                 matched_score += result["meterValue"] + 1
+
+    # (Fuzzy search) One last attempt using reddit-supplied year +/- 1 year
+    if matched_score < 0:
+        prior_year = int(year) - 1
+        next_year = int(year) + 1
+
+        if json_data["movieCount"] > 0:
+            e = '...now trying a rough search in Movies between {prior_year} and {next_year}'.format(title=title, prior_year=prior_year, next_year=next_year)
+            print(e)
+
+            for result in movie_results:
+                result_year = result['year'] if 'year' in result else 0
+                if abs(int(result_year) - int(year)) < 2 and 'meterScore' in result:
+                    matched_score += result["meterScore"] + 1
+        else:
+            e = '...now trying a rough search in TV Shows between {prior_year} and {next_year}'.format(title=title, prior_year=prior_year, next_year=next_year)
+            print(e)
+
+        if json_data["tvCount"] > 0:
+
+            for result in tv_results:
+                startYear = result["startYear"] if 'startYear' in result else 0
+                endYear = result["endYear"] if 'endYear' in result else 0
+
+                if abs(int(startYear) - int(year)) < 2 and 'meterValue' in result:
+                    matched_score += result["meterValue"] + 1
+                if abs(int(endYear) - int(year)) < 2 and 'meterValue' in result:
+                    matched_score += result["meterValue"] + 1
+
+    # return result
     if matched_score > 60:
         return '🍅 {matched_score}'.format(matched_score=matched_score)
 
     elif matched_score >= 0:
         return '💩 {matched_score}'.format(matched_score=matched_score)
-
     else:
-        return 'Cound not find score for {title} ({year})'.format(title=title,year=year)
+        return 'Could not find a score for {title} ({year})'.format(title=title,year=year)
 
 
 for movie in allMovies:
+    print("\n" + term.black_on_white + term.bold + movie.title + term.normal)
+    #print(movie.year)
     rotten_score = find_score(movie.title, movie.year)
-    movie.rt_score = rotten_score
-    print(movie.title)
-    print(movie.year)
-    print(movie.rt_score)
+    #movie.rt_score = rotten_score
+    print(rotten_score)
     time.sleep(2)
--- readme.md
+++ readme.md
@@ -1,15 +1,19 @@
 🎥 Netflix Recommender
 ======================
 
-A simple tool that checks out the subreddit r/NetflixBestOf for US movie recommendations.
-
-It then takes those recommendations and cross references Rotten Tomatoes.
-
-Note: Fandango now has an application process for (official) access to the Rotten Tomatoes API. [More info](https://developer.fandango.com/rotten_tomatoes)
+This is a simple tool that checks out the subreddit r/NetflixBestOf for US movie recommendations. It then takes those recommendations and cross-references Rotten Tomatoes to return rating information.
 
 Here's what you need to do to get this script up and running:
 1. Generate OAuth2 tokens to access Reddit's API. [Detailed Instructions](https://github.com/reddit-archive/reddit/wiki/OAuth2)
 2. Create a config file based on on your_config.yml and insert your API keys.
 3. Run it!
 
-If you want more results you can change the time filter to 'week' or 'month'. (Theoretically you could use 'year', but you really should not given the number of API requests that would generate.)
+If you want more results, you can set change time_window in the config file to 'week' or 'month'. Theoretically you could use 'year', but you really shouldn't given the number of API requests that would likely generate. I have yet to experience any rate limiting issues, but I can only assume that the Rotten Tomatoes API would stop responding after some time. Officially, Fandango has dropped support for public access to the API. They now has an application process for (official) access to the Rotten Tomatoes API. [More info](https://developer.fandango.com/rotten_tomatoes)
+
+Usage Notes:
+------------
+The movie/tv series meta data sourced from Reddit occasionally less than perfect:
+
++ Cross-referencing the title of work between the platforms can be challenging. There are movies with the same (or very similar) titles. Additionally, there are movies that get turned into TV shows and vice-versa.[TV Shows based on Movies](https://en.wikipedia.org/wiki/List_of_television_programs_based_on_films). That's where the year comes into play...
+
++ Cross-referencing the year of a work between the two platforms can also be tough. For films posted on r/NetflixBestOf, the year that gets provided in reddit post title is often the official release year, but sometimes it instead ends up being the year the film was added to (US) Netflix. For TV series, the provided year could be the start date of the first season or the final date of the last season (or anything in that range). When the script fails to find a perfect match exact match for the year, it tries finding a rough match, +/- 1 year.
